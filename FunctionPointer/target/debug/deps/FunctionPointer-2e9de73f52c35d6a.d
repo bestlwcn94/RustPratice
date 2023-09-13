3db517509bf88332f6fd7ee304325f9f929164dd
wewe <simplelly99z@outlook.com>
2023-09-13 23:14:26 +0800

    first commit

---
+++ FunctionPointer/target/debug/deps/FunctionPointer-2e9de73f52c35d6a.d
@@ -0,0 +1,5 @@
+F:\00001.MyProgram\003.Rust\02.MyProgram\RustPratice\FunctionPointer\target\debug\deps\FunctionPointer-2e9de73f52c35d6a.rmeta: src\main.rs
+
+F:\00001.MyProgram\003.Rust\02.MyProgram\RustPratice\FunctionPointer\target\debug\deps\FunctionPointer-2e9de73f52c35d6a.d: src\main.rs
+
+src\main.rs:
